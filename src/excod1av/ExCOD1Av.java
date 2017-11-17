3429f051944db47cebe2949b6c9cdee24b174186
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-11-17 09:57:21 +0100

    Letra F

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -12,7 +12,7 @@ public class ExCOD1Av {
         System.out.println("Texto para commit C en rama exp");
         System.out.println("Texto para commit D en rama exp");
         System.out.println("Texto para commit E en la rama master");
-        System.out.println("Texto para commit F en la rama master con todos los commit");
+        System.out.println("Texto para commit F en la rama master con todos los commit ");
     }
     
 }
